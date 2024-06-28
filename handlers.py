f5582e223058a77b40ed7b6c331d67b3085c05b2
Divaon <56230474+Divaon@users.noreply.github.com>
2024-06-28 16:46:06 +0300

    Add a try-except when processing a voice message

--- handlers.py
+++ handlers.py
@@ -33,12 +33,15 @@ async def start_handler(message: Message):
 async def voice_message_handler(message: Message):
     if message.content_type == ContentType.VOICE:
         voice_file_id = message.voice.file_id
-        mp3_file = await convert_voice_to_mp3(voice_file_id)
-        answer = await get_answer_on_voice_message(mp3_file)
-        answer_file = FSInputFile(answer)
-        await message.answer_audio(audio=answer_file)
-        os.remove(mp3_file)
-        os.remove(answer)
-        await message.answer(f"Я получил голосовое сообщение от вас.")
+        try:
+            mp3_file = await convert_voice_to_mp3(voice_file_id)
+            answer = await get_answer_on_voice_message(mp3_file)
+            answer_file = FSInputFile(answer)
+            await message.answer_audio(audio=answer_file)
+            os.remove(mp3_file)
+            os.remove(answer)
+            await message.answer(f"Я получил голосовое сообщение от вас.")
+        except Exception as e:
+            await message.answer(f"При обработке голосового сообщения возникли проблемы.")
     else:
         await message.answer(f"Твой ID: {message.from_user.id}. Я работаю с голосовыми, а не с текстовыми сообщениями.")
