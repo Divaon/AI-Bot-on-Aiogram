06e3265b6b4b4c23e02d53b7dc4ff8e7fea80f9d
Divaon <56230474+Divaon@users.noreply.github.com>
2024-07-03 21:18:43 +0300

    Work with files by using context manager

--- audio_to_text.py
+++ audio_to_text.py
@@ -3,9 +3,11 @@ from settings import client
 
 # return text from audio file
 async def audio_to_text(audio_file_name):
-    audio_file = open(audio_file_name, "rb")
-    transcription = client.audio.transcriptions.create(
-        model="whisper-1",
-        file=audio_file
-    )
+    async with (audio_file_name, "rb") as audio_file:
+        transcription = client.audio.transcriptions.create(
+            model="whisper-1",
+            file=audio_file
+        )
     return transcription.text
+
+
