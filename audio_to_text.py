86184949b5fc8b8150b87769b8783ae857a9667d
Divaon <56230474+Divaon@users.noreply.github.com>
2024-07-03 22:53:11 +0300

    Fix audio_to_text

--- audio_to_text.py
+++ audio_to_text.py
@@ -1,13 +1,10 @@
 from settings import client
 
 
-# return text from audio file
 async def audio_to_text(audio_file_name):
-    async with (audio_file_name, "rb") as audio_file:
+     with open(audio_file_name, "rb") as audio_file:
         transcription = client.audio.transcriptions.create(
             model="whisper-1",
-            file=audio_file
-        )
-    return transcription.text
-
+            file=audio_file)
+        return transcription.text
 
