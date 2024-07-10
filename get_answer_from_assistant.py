087188d2350351d81d5d43dfe61e17e92d9e735e
Divaon <56230474+Divaon@users.noreply.github.com>
2024-07-10 17:37:27 +0300

    Delete some legacy code
    Delete function wait_on_run and her calls.

--- get_answer_from_assistant.py
+++ get_answer_from_assistant.py
@@ -2,16 +2,6 @@ import time
 
 from settings import client, config
 
-# Waiting in a loop
-async def wait_on_run(run, thread):
-    while run.status == "queued" or run.status == "in_progress":
-        run = client.beta.threads.runs.retrieve(
-            thread_id=thread.id,
-            run_id=run.id,
-        )
-        time.sleep(0.5)
-    return run
-
 
 
 # send question to assistant
@@ -41,8 +31,6 @@ async def create_thread_and_run(user_input):
 # return answer of assistant
 async def get_answer(question):
     thread, run = await create_thread_and_run(question)
-    run = await wait_on_run(run, thread)
-
     while run.status != "completed":
         if run.status in ['expired', 'failed', 'cancelled', 'incomplete']:
             return "Извините. Я не смог обдумать ответ"
